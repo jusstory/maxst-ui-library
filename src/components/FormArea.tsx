9a47df138736b50b1065330103f803d10ec4af2a
jusstory <jjoo8829@naver.com>
2024-01-08 16:06:51 +0900

    form page에 공통 sidebar 적용 및 스타일 커스텀 로직 추가

--- src/components/FormArea.tsx
+++ src/components/FormArea.tsx
@@ -11,14 +11,14 @@ import {
   Divider,
   Dropdown,
   Title,
-  Breadcrumb,
 } from '@maxst-designsystem/maxst-design-system';
 import {
   ArrowDownLineIcon,
   CheckLineIcon,
   ArrowRightLineIcon,
-  UserLineIcon,
 } from '@maxst-designsystem/icons';
+import { MyContainer, MyForm } from '@layout/container/ContainerStyle';
+import Sidebar from '@components/sidebar';
 
 type formDataType = {
   id: string | null;
@@ -79,167 +79,170 @@ function FormArea() {
   };
 
   return (
-    <Form id="form-login">
-      <Title role={2} type={'title'} size={'s'}>
-        Form
-      </Title>
-      <InputGroup
-        fullWidth={true}
-        label="Id"
-        helperText="helper text"
-        required={true}
-        state={idError ? 'error' : 'default'}
-      >
-        <TextField
-          id="default-textfield-01"
-          placeholder="placeholder"
-          resetButton={true}
-          onChange={onChangeId}
-        />
-      </InputGroup>
-      <InputGroup
-        fullWidth={true}
-        label="Password"
-        helperText="helper text"
-        required={true}
-        state={passwordError ? 'error' : 'default'}
-      >
-        <TextField
-          id="default-textfield-03"
-          placeholder="placeholder"
-          password={true}
-          onChange={onChangePassword}
-        />
-      </InputGroup>
-      <InputGroup
-        fullWidth={true}
-        label="Id"
-        helperText="helper text"
-        required={true}
-        state={idError ? 'error' : 'default'}
-      >
-        <TextField
-          id="default-textfield-01"
-          placeholder="placeholder"
-          resetButton={true}
-          onChange={onChangeId}
-        />
-      </InputGroup>
-      <InputGroup
-        fullWidth={true}
-        label="Password"
-        helperText="helper text"
-        required={true}
-        state={passwordError ? 'error' : 'default'}
-      >
-        <TextField
-          id="default-textfield-03"
-          placeholder="placeholder"
-          password={true}
-          onChange={onChangePassword}
-        />
-      </InputGroup>
-      <InputGroup
-        fullWidth={true}
-        label="phone"
-        helperText="helper text"
-        required={true}
-      >
-        <Dropdown
-          menuData={menuData}
-          onChange={onChangeMenu}
-          onClick={onClickMenu}
-          id="phone-01"
-          className="dropdown-example"
+    <MyContainer>
+      <Sidebar />
+      <MyForm id="form-login">
+        <Title role={2} type={'title'} size={'s'}>
+          Form
+        </Title>
+        <InputGroup
+          fullWidth={true}
+          label="Id"
+          helperText="helper text"
+          required={true}
+          state={idError ? 'error' : 'default'}
         >
           <TextField
-            id=""
-            required={true}
+            id="default-textfield-01"
             placeholder="placeholder"
-            iconRight={<ArrowDownLineIcon />}
+            resetButton={true}
+            onChange={onChangeId}
           />
-        </Dropdown>
-        <TextField
-          id="phone-02"
-          placeholder="placeholder"
-          password={true}
-          onChange={onChangePassword}
-        />
-        <TextField
-          id="phone-03"
-          placeholder="placeholder"
-          password={true}
-          onChange={onChangePassword}
-        />
-      </InputGroup>
-      <Checkbox id="login-checkbox" size="l" label="Save Password" />
-      <CheckboxGroup
-        controlMode
-        id="checkbox-all"
-        label="전체선택"
-        onChange={onChangeCheckbox}
-        onClick={onClickCheckbox}
-        options={[
-          {
-            checked: true,
-            id: 'checkbox_01',
-            label: 'label 01',
-            size: 'l',
-          },
-          {
-            checked: false,
-            id: 'checkbox_02',
-            label: 'label 02',
-            size: 'l',
-          },
-          {
-            checked: true,
-            id: 'checkbox_03',
-            label: 'label 03',
-            size: 'l',
-          },
-          {
-            checked: false,
-            id: 'checkbox_04',
-            label: 'label 04',
-            size: 'l',
-          },
-        ]}
-        size="l"
-        type="selected"
-      />
-      <br />
-      <Divider direction="horizontal" type="default" />
-      <ButtonGroup>
-        <Button type="ghost" size="xl">
-          Button
-        </Button>
-        <Button
-          type="primary"
-          size="xl"
-          isActionMode={true}
-          onClick={() => onOpen('modal')}
+        </InputGroup>
+        <InputGroup
+          fullWidth={true}
+          label="Password"
+          helperText="helper text"
+          required={true}
+          state={passwordError ? 'error' : 'default'}
         >
-          Button
-        </Button>
-      </ButtonGroup>
-      <Modal
-        title="Modal"
-        open={isOpenModal}
-        onClose={() => onClose('modal')}
-        isCloseButton={true}
-        mainButton={{ type: 'primary', text: '확인', onClick: onSubmit }}
-        subButton={{ text: '취소', onClick: () => onClose('modal') }}
-        subtlestButton={{
-          text: '',
-          onClick: function (): void {
-            throw new Error('Function not implemented.');
-          },
-        }}
-      >
-        Modal 입니다.
-      </Modal>
-    </Form>
+          <TextField
+            id="default-textfield-03"
+            placeholder="placeholder"
+            password={true}
+            onChange={onChangePassword}
+          />
+        </InputGroup>
+        <InputGroup
+          fullWidth={true}
+          label="Id"
+          helperText="helper text"
+          required={true}
+          state={idError ? 'error' : 'default'}
+        >
+          <TextField
+            id="default-textfield-01"
+            placeholder="placeholder"
+            resetButton={true}
+            onChange={onChangeId}
+          />
+        </InputGroup>
+        <InputGroup
+          fullWidth={true}
+          label="Password"
+          helperText="helper text"
+          required={true}
+          state={passwordError ? 'error' : 'default'}
+        >
+          <TextField
+            id="default-textfield-03"
+            placeholder="placeholder"
+            password={true}
+            onChange={onChangePassword}
+          />
+        </InputGroup>
+        <InputGroup
+          fullWidth={true}
+          label="phone"
+          helperText="helper text"
+          required={true}
+        >
+          <Dropdown
+            menuData={menuData}
+            onChange={onChangeMenu}
+            onClick={onClickMenu}
+            id="phone-01"
+            className="dropdown-example"
+          >
+            <TextField
+              id=""
+              required={true}
+              placeholder="placeholder"
+              iconRight={<ArrowDownLineIcon />}
+            />
+          </Dropdown>
+          <TextField
+            id="phone-02"
+            placeholder="placeholder"
+            password={true}
+            onChange={onChangePassword}
+          />
+          <TextField
+            id="phone-03"
+            placeholder="placeholder"
+            password={true}
+            onChange={onChangePassword}
+          />
+        </InputGroup>
+        <Checkbox id="login-checkbox" size="l" label="Save Password" />
+        <CheckboxGroup
+          controlMode
+          id="checkbox-all"
+          label="전체선택"
+          onChange={onChangeCheckbox}
+          onClick={onClickCheckbox}
+          options={[
+            {
+              checked: true,
+              id: 'checkbox_01',
+              label: 'label 01',
+              size: 'l',
+            },
+            {
+              checked: false,
+              id: 'checkbox_02',
+              label: 'label 02',
+              size: 'l',
+            },
+            {
+              checked: true,
+              id: 'checkbox_03',
+              label: 'label 03',
+              size: 'l',
+            },
+            {
+              checked: false,
+              id: 'checkbox_04',
+              label: 'label 04',
+              size: 'l',
+            },
+          ]}
+          size="l"
+          type="selected"
+        />
+        <br />
+        <Divider direction="horizontal" type="default" />
+        <ButtonGroup align="right" buttonWidth="180px">
+          <Button type="ghost" size="xl">
+            Button
+          </Button>
+          <Button
+            type="primary"
+            size="xl"
+            isActionMode={true}
+            onClick={() => onOpen('modal')}
+          >
+            Button
+          </Button>
+        </ButtonGroup>
+        <Modal
+          title="Modal"
+          open={isOpenModal}
+          onClose={() => onClose('modal')}
+          isCloseButton={true}
+          mainButton={{ type: 'primary', text: '확인', onClick: onSubmit }}
+          subButton={{ text: '취소', onClick: () => onClose('modal') }}
+          subtlestButton={{
+            text: '',
+            onClick: function (): void {
+              throw new Error('Function not implemented.');
+            },
+          }}
+        >
+          Modal 입니다.
+        </Modal>
+      </MyForm>
+    </MyContainer>
   );
 }
 
--- src/layout/container/ContainerStyle.ts
+++ src/layout/container/ContainerStyle.ts
@@ -1,6 +1,6 @@
 // styled-component
 import styled, { css } from 'styled-components';
-import { Container } from '@maxst-designsystem/maxst-design-system';
+import { Container, Form } from '@maxst-designsystem/maxst-design-system';
 
 const MyContainer = styled(Container)`
   ${({ theme }) => {
@@ -10,4 +10,12 @@ const MyContainer = styled(Container)`
   }}
 `;
 
-export { MyContainer };
+const MyForm = styled(Form)`
+  ${({ theme }) => {
+    return css`
+      padding-left: 50px;
+    `;
+  }}
+`;
+
+export { MyContainer, MyForm };
