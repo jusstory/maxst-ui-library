25a569e6528e6810ec9675194565c8af22fa8b4a
jusstory <jjoo8829@naver.com>
2024-01-10 11:34:08 +0900

    next.config.js 설정 추가

--- next.config.js
+++ next.config.js
@@ -2,10 +2,19 @@
 const path = require('path');
 
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
     prependData: `@import "@maxst-designsystem/maxst-design-system/src/styles/scss/abstracts/_variables.scss";`,
+  },
+  pageExtensions: ['tsx'],
+  compiler: {
+    styledComponents: {
+      ssr: true,
+      fileName: true,
+      displayName: true,
+      pure: true,
+    },
   },
 };
 
