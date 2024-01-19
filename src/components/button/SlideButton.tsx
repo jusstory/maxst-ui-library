23011287edcec2a849bf2aa8064cd6cd36c6e2a7
jusstory <jjoo8829@naver.com>
2024-01-19 16:40:22 +0900

    sliderButton 코드 정리

--- src/components/button/SlideButton.tsx
+++ src/components/button/SlideButton.tsx
@@ -4,10 +4,7 @@ import { SlideButtonStyles } from './SliderButtonStyle';
 function SlideButton() {
   return (
     <SlideButtonStyles>
-      <div
-        id="116632343"
-        className="icon-container white close js-signup:closeSvg show"
-      >
+      <div id="slide-button" className="icon-container show">
         <svg
           width="50"
           height="50"
@@ -15,7 +12,7 @@ function SlideButton() {
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <mask className="mask" id="mask-116632343">
+          <mask className="mask" id="mask-slide-button">
             <rect x="0" y="0" width="50" height="50" fill="white"></rect>
             <polygon
               className="mask"
@@ -24,14 +21,14 @@ function SlideButton() {
             ></polygon>
           </mask>
           <defs>
-            <clipPath id="clip-116632343" clipPathUnits="userSpaceOnUse">
+            <clipPath id="clip-slide-button">
               <polygon
                 className="mask"
                 points="0 0, 100 0, 100 100, 0 100"
               ></polygon>
             </clipPath>
           </defs>
-          <g mask="url(#mask-116632343)">
+          <g mask="url(#mask-slide-button)">
             <path
               className="border"
               d="M0.5 12.3806C0.5 11.2234 0.945841 10.1106 1.7449 9.27351L8.79006 1.89286C9.63919 1.0033 10.8154 0.5 12.0452 0.5H45C47.4853 0.5 49.5 2.51472 49.5 5V45C49.5 47.4853 47.4853 49.5 45 49.5H5C2.51472 49.5 0.5 47.4853 0.5 45V12.3806Z"
@@ -39,7 +36,7 @@ function SlideButton() {
               stroke="black"
             ></path>
           </g>
-          <g className="hover" clip-path="url(#clip-116632343)">
+          <g className="hover" clip-path="url(#clip-slide-button)">
             <path
               className="border"
               d="M0.5 12.3806C0.5 11.2234 0.945841 10.1106 1.7449 9.27351L8.79006 1.89286C9.63919 1.0033 10.8154 0.5 12.0452 0.5H45C47.4853 0.5 49.5 2.51472 49.5 5V45C49.5 47.4853 47.4853 49.5 45 49.5H5C2.51472 49.5 0.5 47.4853 0.5 45V12.3806Z"
--- src/components/button/SliderButtonStyle.ts
+++ src/components/button/SliderButtonStyle.ts
@@ -4,37 +4,37 @@ import styled, { css } from 'styled-components';
 export const SlideButtonStyles = styled.div`
   ${({ theme }) => {
     return css`
-      height: 3.125rem;
-      width: 3.125rem;
-      background-color: transparent;
-      border: none;
+      height: 50px;
+      width: 50px;
       padding: 0;
+      border: none;
+      background-color: transparent;
       cursor: pointer;
 
       .icon-container {
-        --main-bg-color: #fff;
+        --bg-color: #fff;
         --hover-color: #000;
-        cursor: pointer;
+
         width: 50px;
         transition: -webkit-clip-path 1s;
         transition: clip-path 1s;
         transition:
           clip-path 1s,
           -webkit-clip-path 1s;
+        cursor: pointer;
 
-        img,
         svg {
           height: auto;
           max-width: 100%;
         }
 
         .mask {
-          transition: transform 0.5s cubic-bezier(0.455, 0.03, 0.515, 0.955);
           stroke: transparent;
           stroke-width: 20px;
+          transition: transform 0.5s cubic-bezier(0.455, 0.03, 0.515, 0.955);
           transform: translateX(-100%) rotate(-145deg) scaleY(0);
           transform-origin: 44% 107%;
-          background-color: black;
+          background-color: var(--hover-color);
         }
 
         &:hover {
@@ -46,13 +46,12 @@ export const SlideButtonStyles = styled.div`
         .border {
           stroke-dasharray: 299px 190px;
           stroke-dashoffset: -190px;
-          fill: var(--main-bg-color);
           stroke: var(--hover-color);
           stroke-width: 1.5;
+          stroke-dashoffset: 0;
           transition: stroke-dashoffset 1s
             cubic-bezier(0.455, 0.03, 0.515, 0.955);
-
-          stroke-dashoffset: 0;
+          fill: var(--bg-color);
         }
         .hover .border {
           fill: var(--hover-color);
