2f198477a110f5e4a0d3b776d619f69b7fbcfd82
jusstory <jjoo8829@naver.com>
2024-02-19 11:23:37 +0900

    ui library v1.0.14 update 및 global style 불필요코드 삭제

--- src/styles/global-style.ts
+++ src/styles/global-style.ts
@@ -237,157 +237,16 @@ export const GlobalStyle = createGlobalStyle`${css`
     white-space: pre-wrap;
   }
 
-  .ant-layout {
-    background-color: ${colors.white};
-  }
-
-  .ant-typography {
-    color: ${colors.gray.darker};
-  }
-
-  // checkbox
-  .ant-checkbox-wrapper:hover .ant-checkbox-inner,
-  .ant-checkbox:hover .ant-checkbox-inner,
-  .ant-checkbox-input:focus + .ant-checkbox-inner,
-  .ant-checkbox-checked::after {
-    border-color: ${colors.primary.default};
-  }
-
-  .ant-checkbox-checked .ant-checkbox-inner {
-    background-color: ${colors.primary.default};
-    border-color: ${colors.primary.default};
-  }
-
-  .ant-checkbox-indeterminate .ant-checkbox-inner::after {
-    background-color: ${colors.primary.default};
-  }
-
-  // paging
-  .ant-pagination-item-active,
-  .ant-pagination-item:hover {
-    border-color: ${colors.primary.default};
-  }
-
-  .ant-pagination-item-active a,
-  .ant-pagination-item-active:hover a {
-    color: ${colors.primary.default};
-  }
-
-  // form
-  .ant-input,
-  .ant-input-group-addon,
-  .ant-select:not(.ant-select-customize-input) .ant-select-selector {
-    border-color: ${colors.gray.low};
-  }
-
   // form input 안내/에러 메세지
   form {
     div {
       color: ${colors.gray.mid};
-
-      &.ant-form-item-explain-error {
-        color: ${colors.alert};
-      }
     }
-  }
-
-  .ant-form-item-explain,
-  .ant-form-item-extra {
-    font-size: 12px;
-  }
-
-  // tooltip
-  .ant-tooltip-inner {
-    padding: 10px;
-    color: ${colors.gray.darker};
-    text-align: center;
-    border: 1px solid ${colors.gray.darker};
-    box-shadow: none;
-    background-color: ${colors.white};
-  }
-
-  .ant-tooltip-arrow {
-    display: none;
-  }
-
-  // dropdown menu
-  .ant-dropdown {
-    min-width: 118px !important;
-
-    & > ul {
-      padding: 4px 0;
-      border: 1px solid ${colors.gray.darker};
-
-      .ant-menu-item {
-        height: auto;
-        padding: 5px 12px;
-        margin: 0;
-        line-height: 22px;
-
-        &:hover,
-        &:focus,
-        &:active {
-          background-color: ${theme.colors.hover};
-        }
-
-        button,
-        a {
-          width: 100%;
-          height: 100%;
-          text-align: left;
-          line-height: 1;
-
-          &:hover,
-          &:focus,
-          &:active {
-            color: ${theme.colors.gray.darker};
-          }
-        }
-
-        &:not(:last-child) {
-          margin-bottom: 0;
-        }
-      }
-    }
-
-    .ant-menu:not(.ant-menu-horizontal) .ant-menu-item-selected {
-      background-color: ${theme.colors.hover};
-    }
-  }
-
-  // table
-  .ant-table-thead > tr > th,
-  .ant-table tfoot > tr > th {
-    padding: 11px 16px;
-  }
-
-  .ant-table-tbody > tr > td,
-  .ant-table tfoot > tr > td {
-    padding: 20px 16px;
-  }
-
-  // button
-  .ant-btn-text {
-    border: 0 none;
-  }
-
-  .ant-typography mark {
-    color: ${colors.alert};
-    background-color: transparent;
   }
 
   // summernote
   .note-editor.note-frame .note-statusbar {
     display: none;
-  }
-
-  // massage
-  .ant-message .anticon {
-    vertical-align: text-bottom;
-
-    & + span {
-      vertical-align: middle;
-    }
   }
 
   .primary-text {
@@ -402,18 +261,6 @@ export const GlobalStyle = createGlobalStyle`${css`
   }
 
   ${media.tablet} {
-    .ant-dropdown {
-      max-width: 320px;
-      width: 100%;
-      padding: 0 30px;
-      left: 50%;
-
-      ul {
-        // 공간맵 저작도구 dropdown option
-        &.authoring_tool_dropdown_option {
-        }
-      }
-    }
   }
 `}
 `;
